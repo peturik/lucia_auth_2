01b24038f04719653f1284e35e6247b674b44569
peturik <peturik@gmail.com>
2025-01-26 15:53:25 +0100

    fixed themeStore and fixed where run pnpm build

--- src/stores/useThemeStore.tsx
+++ src/stores/useThemeStore.tsx
@@ -7,25 +7,6 @@ interface ThemeState {
   setTheme: (theme: string) => void;
 }
 
-// export const useThemeStore = create<ThemeState>()((set) => {
-//   const storedTheme = localStorage.getItem("theme");
-//   const prefersDark =
-//     typeof window !== "undefined" &&
-//     window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-//   const initialTheme =
-//     storedTheme && storedTheme !== "system"
-//       ? storedTheme
-//       : prefersDark
-//       ? "dark"
-//       : "light";
-
-//   return {
-//     theme: initialTheme, // Значення за замовчуванням
-//     setTheme: (theme) => set({ theme }),
-//   };
-// });
-
 export const useThemeStore = create<ThemeState>()(
   persist(
     (set) => ({
